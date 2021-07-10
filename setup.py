40f5e439c3feb3c46b19a3f1e00c30acbe9de47b
ntony3419 <ntony3419@gmail.com>
2021-07-10 10:53:13 +0800

    update location for browser functions

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='optimizedselenium',
+    packages=find_packages(),
+    version='0.1.0',
+    description='generate selenium driver and common action based on selenium',
+    author='ntony',
+    license='GPL',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='test',
+
+)
