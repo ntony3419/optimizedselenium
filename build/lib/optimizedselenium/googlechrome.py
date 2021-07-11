7c091799bf748a6d81800756f1a6e20b67a93c6d
ntony3419 <ntony3419@gmail.com>
2021-07-11 16:12:47 +0800

    move location of some function to common.py

--- build/lib/optimizedselenium/googlechrome.py
+++ build/lib/optimizedselenium/googlechrome.py
@@ -1,3 +1,5 @@
+import traceback
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -24,8 +26,10 @@ class Chrome_Browser():
         #     self.options.add_argument("--window-size=1920,1080")
         # if maximize is True:
         #     self.options.add_argument("--start-maximized")
+        self.window_size = None
         if window_size is not None:
             self.window_size = window_size.split(",")
+
         self.maximize = False
         if maximize is True:
             self.maximize = True
@@ -49,14 +53,19 @@ class Chrome_Browser():
             driver = webdriver.Chrome(executable_path=self.driver_path, options=self.options)
             if self.maximize is True:
                 driver.maximize_window()
-            if self.winidow_size is not None and self.maximize is False:
+            if self.window_size is not None and self.maximize is False:
                 driver.set_window_size(self.window_size[1],self.window_size[0])
-        except:
-            raise Exception(f"Unable to open browser with according profile."
-                             f"Possible issue and solution: "
-                             f"\n- Profile is in used: Close all the google chrome tabs to release the profile"
-                             f"\n- Chrome Driver doesn't exist: download chrom driver associate with chrome version from https://chromedriver.chromium.org/downloads"
-                             f"\n- Chrome Driver version different: download new chrome driver assosicate with chrome version from https://chromedriver.chromium.org/downloads")
+
+        except (WebDriverException, FileNotFoundError):
+            print(f"Can't find the executable <.exe> of google chrome at <{self.driver_path}> !!\nMake sure the executable file and path to its executable in setting.conf match each other")
+            exit()
+
+            #print(traceback.format_exc())
+            # raise Exception(f"Unable to open browser with according profile."
+            #                  f"Possible issue and solution: "
+            #                  f"\n- Profile is in used: Close all the google chrome tabs to release the profile"
+            #                  f"\n- Chrome Driver doesn't exist: download chrom driver associate with chrome version from https://chromedriver.chromium.org/downloads"
+            #                  f"\n- Chrome Driver version different: download new chrome driver assosicate with chrome version from https://chromedriver.chromium.org/downloads")
         return driver
 
     def scroll_down(self,browser_driver, amount):
